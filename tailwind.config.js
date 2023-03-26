8d0d94048f9679ad447e944bc896120697fa5e82
A-Abd <abrar.abdulwahed@gmail.com>
2023-03-26 11:09:09 +0300

    fix: prettier formatter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/pages/**/*.{js,jsx}",   
-             "./src/components/**/*.{js,jsx}",  
-  ],
-  purge: ['./src/pages/**/*.{js,jsx}', './src/components/**/*.{js,jsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/pages/**/*.{js,jsx}", "./src/components/**/*.{js,jsx}"],
+    purge: ["./src/pages/**/*.{js,jsx}", "./src/components/**/*.{js,jsx}"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
