e53f7129a091fa1bf7f79a32c2cef5a00c81db18
A-Abd <abrar.abdulwahed@gmail.com>
2023-03-27 23:36:39 +0300

    feat: container for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,20 +4,22 @@ import { navigation } from "@/constants";
 export default function Footer() {
     return (
         <>
-            <div className='flex flex-col md:flex-row'>
-                <div>subscribe</div>
-                <nav>
-                    <ul className='flex'>
-                        {navigation.map((nav) => {
-                            return (
-                                <li key={nav.name}>
-                                    <Link href={nav.href}>{nav.name}</Link>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </nav>
-            </div>
+            <footer className='bg-yellow py-8'>
+                <div className='container flex flex-col md:flex-row justify-center md:justify-between'>
+                    <div>subscribe</div>
+                    <nav className=''>
+                        <ul className='flex text-light-gray'>
+                            {navigation.map((nav) => {
+                                return (
+                                    <li key={nav.name} className='mx-6'>
+                                        <Link href={nav.href}>{nav.name}</Link>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </nav>
+                </div>
+            </footer>
         </>
     );
 }
