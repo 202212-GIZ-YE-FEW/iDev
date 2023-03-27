9256e5869d613e703d4e25fbf5bdeba73905ac9c
A-Abd <abrar.abdulwahed@gmail.com>
2023-03-28 01:23:33 +0300

    chore: save after merge

--- src/components/__test__/Footer.test.js
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer";
-
-import Footer from "../Footer";
-
-it("renders correctly", () => {
-    const tree = renderer.create(<Footer />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
