dc3f58ea9a7b7d7dbddee8d028d89ea046be148f
A-Abd <abrar.abdulwahed@gmail.com>
2023-03-28 00:00:51 +0300

    chore: update in font-size of theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,6 +43,7 @@ module.exports = {
             sans: ["Poppins", "Graphik", "sans-serif"],
         },
         fontSize: {
+            
             xl: [
                 "1.25rem", // 20px
                 {
