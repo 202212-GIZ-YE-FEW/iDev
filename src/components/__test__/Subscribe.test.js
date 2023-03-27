21588033e0a3b82cb8eadc44f50fc28efbc8aa5b
A-Abd <abrar.abdulwahed@gmail.com>
2023-03-27 16:13:43 +0300

    test: unit test for subscribe

---
+++ src/components/__test__/Subscribe.test.js
@@ -0,0 +1,8 @@
+import renderer from "react-test-renderer";
+
+import Subscribe from "./../Subscribe";
+
+it("renders correctly", () => {
+    const tree = renderer.create(<Subscribe />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
