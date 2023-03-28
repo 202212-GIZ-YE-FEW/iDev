8c46ad247a6649d4510b3d14c6285a19304f6173
Jehad Almaliki <yourgitemail@example.com>
2023-03-28 13:00:33 +0300

    refactor: enhance according to change request of reveiw

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -10,46 +10,17 @@ const Input = (props) => {
         isRequired = false,
         placeholder,
         label,
-        position = "top",
-        labelFontSize = "5xl",
-        labelFontWeight = "normal",
-        radius = "md",
-        inputWidthSize = "",
-        inputHightSize = "",
         inputFontSize = "3xl",
         inputFontWeight = "light",
 
         ...rest
     } = props;
 
-    const inputContainerClasses =
-        position === "left" || position === "right"
-            ? "flex items-center "
-            : "block ";
+    const inputClasses = `border border-solid border-gray-300 px-[20px] py-[20px]  text-${inputFontSize} font-${inputFontWeight}`;
 
-    const inputClasses = `border border-solid border-gray-300 px-[20px] py-[20px] rounded-${radius} 
-    ${inputWidthSize ? `w-${inputWidthSize} ` : ""} ${
-        inputHightSize ? `h-${inputHightSize} ` : ""
-    } 
-  
-  text-${inputFontSize} font-${inputFontWeight}`;
-
-    const inputStyles = {
-        width: inputWidthSize,
-        height: inputHightSize,
-    };
     return (
-        <div className={` ${inputContainerClasses}`}>
-            {label && (
-                <label
-                    htmlFor={rest.id || rest.name}
-                    className={`font-poppins font-normal  text-${labelFontSize} ${
-                        position === "left" ? "mr-10 " : "mb-2"
-                    } font-${labelFontWeight} flex items-center `}
-                >
-                    {label}
-                </label>
-            )}
+        <>
+            {label && <label htmlFor={rest.id || rest.name}>{label}</label>}
             <input
                 onChange={handleChange}
                 value={value}
@@ -59,9 +30,8 @@ const Input = (props) => {
                 required={isRequired}
                 placeholder={placeholder}
                 className={inputClasses}
-                style={inputStyles}
             />
-        </div>
+        </>
     );
 };
 export default Input;
@@ -75,7 +45,6 @@ label="email"
  placeholder="name"
  inputWidthSize="100px" 
  inputHightSize="40px"
- labelFontWeight="normal"
 
 /> */
 }
