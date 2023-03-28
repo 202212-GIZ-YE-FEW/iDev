813393987134e25add0e1e1ad96a2afdc4429f12
Jehad Almaliki <yourgitemail@example.com>
2023-03-28 16:58:01 +0300

    feat: design login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Login() {
+    return (
+        <div className='flex'>
+            <div className='w-1/2 p-4'>
+                <form></form>
+            </div>
+            <div className='w-1/2 p-4'></div>
+        </div>
+    );
+}
