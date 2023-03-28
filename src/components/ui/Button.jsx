03e29017dd902302edd2cc84aab8e6ef8727200f
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-28 17:21:27 +0300

    chore: update the button component
    - onClick attr has been added.
    - Medium size has been added.

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+
 export default function Button(prop) {
     const {
         content,
@@ -7,14 +8,17 @@ export default function Button(prop) {
         textTransform = "capitalize",
         fontSize = "xl",
         radius = "6px",
+        onClick,
     } = prop;
     return (
         <button
+            onClick={onClick}
             className={clsx(
                 `font-normal cursor-pointer ${textTransform} text-${fontSize} rounded-${radius}`,
                 {
                     "px-[28px] py-[4.7px]": size === "small",
                     "px-[59px] py-[12px] font-medium": size === "large",
+                    "px-[40px] py-[8px]": size === "medium",
                     "bg-cyan text-black": filled === "true",
                     "border-2 border-cyan text-cyan": filled === "false",
                 }
