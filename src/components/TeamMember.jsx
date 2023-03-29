6edb39fd1c0eaa929930fa0c7362bd69276040c4
MohdBasurra <mohdye96@gmail.com>
2023-03-29 21:31:22 +0300

    feat(team section): created Team section with it`s test
    Team section contain Team member cards

--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -8,7 +8,7 @@ const TeamMember = ({ title, job, photo }) => {
                 <Image src={photo} alt='My Image' width={176} height={190} />
                 <div className='bg-black w-full h-[5px] mt-0 bg-phosphorescent '></div>
                 <div className='p-5'>
-                    <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white pb-[33px]'>
+                    <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white pb-[33px] h-[50px] mb-[40px]'>
                         {title}
                     </h5>
                     <p className='mb-3 font-normal text-gray-700 dark:text-gray-400 pb-[40px]'>
