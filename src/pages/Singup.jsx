04afb4503b5e253f46cb3a641b3a0c296cf67353
Jehad Almaliki <yourgitemail@example.com>
2023-03-29 22:55:59 +0300

    feat: make it responsive in all screen

--- src/pages/Singup.jsx
+++ src/pages/Singup.jsx
@@ -8,14 +8,14 @@ export default function Singup() {
     return (
         <div className='h-full'>
             <div className='g-6 flex h-full flex-wrap items-center lg:justify-center mx-12 my-8 justify-between   space-x-2'>
-                <div className='shrink-1 mb-12 mt-12 grow-0 basis-auto  md:mb-0 px-10  md:w-12/12 md:shrink-0 lg:w-8/12 xl:w-7/12'>
+                <div className='shrink-1 mb-12 mt-12 grow-0 basis-auto  md:mb-0 lg:px-20 xl:px-0 md:w-5/12 md:shrink-0 lg:w-8/12 xl:w-7/12'>
                     <img
                         src='https://i.ibb.co/RSfKc5F/Signup-Image.png'
                         className='w-full'
                         alt='Sample image'
                     />
                 </div>
-                <div className='w-full max-w-xs  mb-12 md:mb-2  md:w-12/12 lg:w-4/12 xl:w-5/12  ml-2 leading-normal '>
+                <div className='w-full max-w-xs md:mx-auto mb-12 md:mb-0  md:shrink-0 md:w-7/12 lg:w-4/12 xl:w-5/12  ml-2 leading-normal '>
                     <FormTitle title='Singup Now' />
                     <form className=' shadow-[0_4px_9px_-4px_#3b71ca] mt-8  p-6'>
                         <div
