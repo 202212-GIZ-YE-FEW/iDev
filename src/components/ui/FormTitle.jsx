4e81977fcccacdcaac12842a7994ce3ae7dcb079
Jehad Almaliki <yourgitemail@example.com>
2023-03-29 22:55:08 +0300

    feat: edit text size in large screen

--- src/components/ui/FormTitle.jsx
+++ src/components/ui/FormTitle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function FormTitle({ title }) {
     return (
         <>
-            <span className='font-poppins  uppercase font-normal font-400 text-3xl leading-75 text-center tracking-tighter'>
+            <span className='font-poppins  uppercase font-normal font-400  md:text-xl lg:text-3xl  leading-75 text-center tracking-tighter'>
                 {title}
             </span>
         </>
