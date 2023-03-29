c7a14f2fb324b363b92d14fbe3290d48e12271ce
A-Abd <abrar.abdulwahed@gmail.com>
2023-03-29 13:25:48 +0300

    feat: putting components to others

--- src/pages/thanks.jsx
+++ src/pages/thanks.jsx
@@ -1,15 +1,22 @@
 import PageIntro from "@/components/PageIntro";
 import Button from "@/components/ui/Button";
+
+import Layout from "@/layout/Layout";
 export default function Thanks(prop) {
     const {
         subtitle = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
     } = prop;
     return (
-        <>
-            <div className='container'>
+        <Layout>
+            <div className='container my-20'>
                 <PageIntro title='Thank you!' subtitle={subtitle} />
-                <Button content='back to home' textTransform='uppercase' />
+                <Button
+                    content='back to home'
+                    textTransform='uppercase'
+                    radius='md'
+                    fontSize='text-lg md:text-xl lg:text-2xl'
+                />
             </div>
-        </>
+        </Layout>
     );
 }
