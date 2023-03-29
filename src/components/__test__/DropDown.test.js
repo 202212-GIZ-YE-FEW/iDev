e5d6b7d7c7d68edf76f5b0a234b688731a0c9b28
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2023-03-29 17:39:07 +0300

    fix: delete snapshot file

--- src/components/__test__/DropDown.test.js
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer";
-
-import Dropdown from "@/components/Dropdown/Dropdown";
-
-it("renders correctly", () => {
-    const tree = renderer.create(<Dropdown />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
