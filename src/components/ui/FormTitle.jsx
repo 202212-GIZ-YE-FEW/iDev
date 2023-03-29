350dc4f8c34c5a83482cd63b5cbb58b9200b79a3
Jehad Almaliki <yourgitemail@example.com>
2023-03-29 21:04:22 +0300

    feat: edit font text size

--- src/components/ui/FormTitle.jsx
+++ src/components/ui/FormTitle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function FormTitle({ title }) {
     return (
         <>
-            <span className='font-poppins  uppercase font-normal font-400 text-5xl leading-75 text-center tracking-tighter'>
+            <span className='font-poppins  uppercase font-normal font-400 text-3xl leading-75 text-center tracking-tighter'>
                 {title}
             </span>
         </>
