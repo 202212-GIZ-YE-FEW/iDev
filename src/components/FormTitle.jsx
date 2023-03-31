ac8c29cd2911eab50e4bfe454dd178583e02b7cc
Jehad Almaliki <yourgitemail@example.com>
2023-03-31 12:01:03 +0300

    feat: translate title to ar

--- src/components/FormTitle.jsx
+++ src/components/FormTitle.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useTranslation } from "next-i18next";
 
 export default function FormTitle({ title }) {
+    const { t } = useTranslation("common");
     return (
         <>
             <span className=' uppercase font-normal  md:text-xl lg:text-3xl  leading-75 text-center tracking-tighter'>
-                {title}
+                {t(title)}
             </span>
         </>
     );
