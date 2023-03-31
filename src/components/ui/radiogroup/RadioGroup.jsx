dc46478c2dd68ac9d062745ca3729763e65c5586
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-31 15:15:01 +0300

    fix: add validation for children component in radio group

--- src/components/ui/radiogroup/RadioGroup.jsx
+++ src/components/ui/radiogroup/RadioGroup.jsx
@@ -3,11 +3,20 @@ import { Children } from "react";
 
 export default function RadioGroup(props) {
     const { title, children, asButton = false } = props;
+    React.Children.forEach(children, (child) => {
+        if (child.type.name !== "RadioInputItem") {
+            throw new Error(
+                "RadioGroup component can only have RadioInputItem as children"
+            );
+        }
+    });
     return (
         <div className='p-4'>
-            <p className='mb-4 p-2 text-3xl font-normal break-words text-black'>
-                {title}
-            </p>
+            {title && (
+                <p className='mb-4 p-2 text-3xl font-normal break-words text-black'>
+                    {title}
+                </p>
+            )}
             <fieldset className='space-y-4'>
                 <div className='text-9xl font-normal text-black'>
                     {Children.map(children, (child) => {
