92e8001b3d2cdffc0a279d5d990a843c96d9efff
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2023-04-01 15:29:27 +0300

    docs: component test pass

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,9 +3,9 @@ import Navbar from "@/components/Navbar";
 export default function Layout({ children }) {
     return (
         <>
-            <Navbar />
+            {/* <Navbar /> */}
             {children}
-            <Footer />
+            {/* <Footer /> */}
         </>
     );
 }
