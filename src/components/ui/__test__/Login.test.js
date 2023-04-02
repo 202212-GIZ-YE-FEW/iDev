2a986b26eac343709e8a99924d8e1fccc59bad4e
Jehad Almaliki <yourgitemail@example.com>
2023-04-02 17:01:53 +0300

    feat: remove login test file

--- src/components/ui/__test__/Login.test.js
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer";
-
-import Login from "@/pages/login";
-
-it("renders correctly", () => {
-    const tree = renderer.create(<Login />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
