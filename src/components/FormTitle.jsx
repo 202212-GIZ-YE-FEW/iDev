71d6b564189b36f861eb732a5988fa58d580663a
Jehad Almaliki <yourgitemail@example.com>
2023-04-02 10:23:28 +0300

    ci: no change happened

--- src/components/FormTitle.jsx
+++ src/components/FormTitle.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
 export default function FormTitle({ title }) {
     const { t } = useTranslation("common");
     return (
@@ -10,12 +10,4 @@ export default function FormTitle({ title }) {
             </span>
         </>
     );
-}
-export async function getStaticProps({ locale }) {
-    return {
-        props: {
-            ...(await serverSideTranslations(locale, ["common"])),
-            // Will be passed to the page component as props
-        },
-    };
 }
