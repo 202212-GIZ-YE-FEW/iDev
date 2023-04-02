b79a4f6d346d879069389ec0b5195cca23a9f290
MohdBasurra <119960587+MohdBasurra@users.noreply.github.com>
2023-04-03 00:22:59 +0300

    Update About.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,60 +1,59 @@
+import TeamMember from "@/components/TeamMember";
+import { useState } from "react";
+import { i18n, useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Image from "next/image";
-const About = () => {
+
+import OurFounding from "public/OurFoundingPayan.png";
+
+import PageIntro from "@/components/PageIntro";
+import Layout from "@/layout/Layout";
+
+export default function About() {
+    const { t } = useTranslation("about");
+
     return (
-        <div>
-            {/* Healing Part */}
-            <div className='flex flex-col h-[518]'>
-                <div>
-                    {/*  Healing 
-     (the subject is missing It`s component we will post it later) */}
+        <>
+            {/* Page Intro  */}
+            <div className=' container mt-8 '>
+                <div className='flex-wrap nowrap md:text-start  text-center justify-center items-center sm:text-center'>
+                    <PageIntro
+                        title={t("healing")}
+                        subtitle={t("BringingHopeThroughTherapy")}
+                    />
                 </div>
-                <div className=' h-[294px] flex justify-center items-center'>
-                    {/*   Second div content goes here  */}
-                    <div className='h-[234px] w-[1096px]'>
-                        <p className='   leading-[2rem] mt-[30px]'>
-                            At Healing, we believe there is a better way to do
-                            things. A morevaluable way where customers are
-                            earned rather than bought. Were obsessively
-                            passionate about it, and our mission is to help
-                            people achieve it. We focus on search engine
-                            optimization. Its one of the least understood and
-                            least transparent aspects of great marketing, and we
-                            see that as an opportunity. Were excited to simplify
-                            SEO for everyone through our software, education,
-                            and community.
-                        </p>
-                    </div>
+
+                <div className='text-start sm:text-center sm:w-full  text-gray  text-xl md:mb-[120px] mb-8 '>
+                    <p>{t("introAboutPage")}</p>
                 </div>
             </div>
-            {/* Our Founding Part */}
-            <div className='flex flex-col md:flex-row mw-[1440px] h-[400px] md:gap-[200px] md:mw-[1024px] gap-[66px] bg-light-cyan'>
-                <div className='w-full md:w-[590px] h-[320px]'>
+
+            <div className='flex flex-col md:flex-row bg-light-cyan '>
+                <div className='w-full md:w-1/3 flex md:justify-end sm:justify-center order-2 md:order-1 p-4'>
                     <Image
-                        className='mb-10 mt-[40px] md:ml-[191px] md:mb-[40px]'
-                        src='/./../public/OurFounding.png'
+                        src={OurFounding}
                         alt='My Image'
                         width={400}
                         height={320}
                     />
                 </div>
-                <div className='w-full md:w-[950px] mt-[66px]'>
-                    <h1 className='bg-red w-full md:w-[363px] h-[47px] text-3xl font-sans mb-4 md:mb-0'>
-                        Our Founding
-                    </h1>
-                    <p className='w-full md:w-[641px] h-[195px] leading-[2.5rem]'>
-                        Healing was founded by Payam Abubakr in 2021. It was
-                        called Healing Online, and started as a blog and an
-                        online community where some of the worlds therapists
-                        shared their research and ideas. We launched the
-                        Beginners Guide to Therapy and our first study, and that
-                        hub of industry expertise transformed into a small
-                        consulting firm and led us to create the Online
-                        Therapist of today!
-                    </p>
+                <div className='w-full md:w-2/3 p-4 flex flex-col order-1 md:order-2  '>
+                    <div className='  lg:w-3/4 w-full text-start   '>
+                        <PageIntro
+                            title={t("OurFounding")}
+                            subtitle={t("AboutHealing")}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
-};
-
-export default About;
+}
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["about", "common"])),
+            // Will be passed to the page component as props
+        },
+    };
+}
