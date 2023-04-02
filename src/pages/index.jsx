e774cda84290235c6cd6073a9994f1e37589b23a
Jehad Almaliki <yourgitemail@example.com>
2023-04-02 23:40:19 +0300

    ci: formate document

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,6 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import Hero from "@/components/Hero";
 import TrustTherapists from "@/components/TrustTherapists";
-
 export default function HomePage() {
     return (
         <>
@@ -11,7 +10,6 @@ export default function HomePage() {
         </>
     );
 }
-
 export async function getStaticProps({ locale }) {
     return {
         props: {
