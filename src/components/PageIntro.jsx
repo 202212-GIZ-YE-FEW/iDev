831b4c5374ace57ceaa2ca17978cf55b99528d1b
Jehad Almaliki <yourgitemail@example.com>
2023-04-02 22:03:24 +0300

    feat: formate file

--- src/components/PageIntro.jsx
+++ src/components/PageIntro.jsx
@@ -1,9 +1,11 @@
+import { useTranslation } from "next-i18next";
 export default function PageIntro(prop) {
     const { title, subtitle } = prop;
+    const { t } = useTranslation("signup");
     return (
         <>
-            <span className='font-normal text-3xl md:text-4xl lg:text-5xl block uppercase'>
-                {title}
+            <span className='font-normal text-2xl md:text-3xl lg:text-2xl block uppercase'>
+                {t(title)}
             </span>
             {subtitle && (
                 <p className='text:lg md:text:xl text-2xl text-black/50 mb-5'>
