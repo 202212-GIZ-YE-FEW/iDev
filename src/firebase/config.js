ffb6401e405d579db6458a5a712f6a14de8e66b4
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-04-05 22:08:40 +0300

    fix: firebase configuration for app

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+import { getApps, initializeApp } from "firebase/app";
+import {
+    FacebookAuthProvider,
+    getAuth,
+    GoogleAuthProvider,
+} from "firebase/auth"; //importing authentication service here
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+export const auth = getAuth(app);
+export const db = getFirestore(app)
+export const googleProvider = new GoogleAuthProvider(); //Google method
+export const facebookProvider = new FacebookAuthProvider(); //faceBook method
