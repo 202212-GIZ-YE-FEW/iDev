f9a887791b7d2d7cbba4ea8d29d622b6aef1c1ae
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-04-05 22:10:51 +0300

    feat: fetch requirements data from firebase collection

--- src/pages/requirements.jsx
+++ src/pages/requirements.jsx
@@ -1,40 +1,62 @@
 import Head from "next/head";
+import Image from "next/image";
 import { withTranslation } from "next-i18next";
+import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import RequirementSVG from "public/requirements.svg";
+import { useEffect, useState } from "react";
 
 import PageIntro from "@/components/PageIntro";
 import Requirement from "@/components/Requirement";
 import RequirementsSection from "@/components/RequirementsSection";
 
-function Requirements({ t }) {
+import getDocument from "@/firebase/getData";
+
+function Requirements({ t, requirements }) {
+    const [requirement, setRequirement] = useState([]);
+    const { i18n } = useTranslation("requirements");
+
+    useEffect(() => {
+        const data = requirements.filter((item) => item.id === i18n.language);
+        const dataArr = data[0].content.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+        setRequirement(dataArr);
+    }, [i18n.language, requirements]);
+
     return (
         <>
             <Head>
                 <title>{t("requirements")}</title>
             </Head>
-            <div className='container mt-8'>
-                <PageIntro title={t("workWithHealing")} />
-                <Requirement
-                    title={t("reliableIncome")}
-                    subtitle={t("reliableIncomeDesc")}
-                />
-                <Requirement
-                    title={t("focusCounseling")}
-                    subtitle={t("focusCounselingDesc")}
-                />
-                <Requirement
-                    title={t("focusCounseling")}
-                    subtitle={t("focusCounselingDesc")}
-                />
-                <RequirementsSection />
+            <div className='container mt-8 grid grid-cols-1 lg:grid-cols-2'>
+                <div className='grid-cols-9 lg:justify-self-start'>
+                    <PageIntro title={t("workWithHealing")} />
+                    <Requirement
+                        title={t("reliableIncome")}
+                        subtitle={t("reliableIncomeDesc")}
+                    />
+                    <Requirement
+                        title={t("focusCounseling")}
+                        subtitle={t("focusCounselingDesc")}
+                    />
+                    <Requirement
+                        title={t("focusCounseling")}
+                        subtitle={t("focusCounselingDesc")}
+                    />
+                    <RequirementsSection requirements={requirement} />
+                </div>
+                <div className='grid-cols-3 lg:justify-self-end py-10'>
+                    <Image src={RequirementSVG} alt='requirements' />
+                </div>
             </div>
         </>
     );
 }
 
 export async function getStaticProps({ locale }) {
+    const requirements = await getDocument("requirements");
     return {
         props: {
+            requirements,
             ...(await serverSideTranslations(locale, [
                 "common",
                 "requirements",
@@ -42,4 +64,5 @@ export async function getStaticProps({ locale }) {
         },
     };
 }
+
 export default withTranslation("requirements")(Requirements);
