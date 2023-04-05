23e79e0d9a8abf798d2444ba2051716176a81ab6
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-04-05 22:13:26 +0300

    feat: add get data from firebase abstraction

---
+++ src/firebase/getData.js
@@ -0,0 +1,13 @@
+import { collection, getDocs, query } from 'firebase/firestore'
+
+import { db } from './config';
+
+export default async function getDocument(doc, criterions) {
+  const q = query(collection(db, doc));
+  const querySnapshot = await getDocs(q);
+  const data = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }))
+  return data;
+}
