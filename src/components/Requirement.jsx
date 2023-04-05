9dab810c4e339e5d99673cbecd2a0676bfb33ca7
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-04-05 22:09:44 +0300

    fix: responsive for requirement component

--- src/components/Requirement.jsx
+++ src/components/Requirement.jsx
@@ -2,7 +2,7 @@ export default function Requirement(props) {
     const { title, subtitle } = props;
     return (
         <div className='flex flex-col md:flex-row mb-5'>
-            <div className='w-full md:w-2/3'>
+            <div className='w-full'>
                 <span className='font-semibold text-xl uppercase'>{title}</span>
                 <p className='font-normal text-md lg:text-xl text-gray capitalize'>
                     {subtitle}
