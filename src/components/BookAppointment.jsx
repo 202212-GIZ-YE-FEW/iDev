ea67f53fa2003849313df57da41dc4d78c529b54
A-Abd <abrar.abdulwahed@gmail.com>
2023-04-05 08:46:01 +0300

    refactor: translate a title of radio group

--- src/components/BookAppointment.jsx
+++ src/components/BookAppointment.jsx
@@ -92,8 +92,7 @@ const TherapyBefore = ({ t }) => {
 };
 
 const SpecificQualities = ({ t }) => {
-    const [specificQualities, setSpecificQualities] =
-        useState("male_counselor");
+    const [specificQualities, setSpecificQualities] = useState("maleCounselor");
     return (
         <>
             <RadioGroup title={t("specificQualitiesCounselor")}>
@@ -101,32 +100,32 @@ const SpecificQualities = ({ t }) => {
                     id='male-counselor'
                     name={specificQualities}
                     title={t("maleCounselor")}
-                    value='male_counselor'
-                    checked={specificQualities === "male_counselor"}
+                    value='maleCounselor'
+                    checked={specificQualities === "maleCounselor"}
                     onChange={(e) => setSpecificQualities(e.target.value)}
                 />
                 <RadioInputItem
                     id='female-counselor'
                     name={specificQualities}
                     title={t("femaleCounselor")}
-                    value='female_counselor'
-                    checked={specificQualities === "female_counselor"}
+                    value='femaleCounselor'
+                    checked={specificQualities === "femaleCounselor"}
                     onChange={(e) => setSpecificQualities(e.target.value)}
                 />
                 <RadioInputItem
                     id='older-counselor'
                     name={specificQualities}
                     title={t("olderCounselor")}
-                    value='older_counselor'
-                    checked={specificQualities === "older_counselor"}
+                    value='olderCounselor'
+                    checked={specificQualities === "olderCounselor"}
                     onChange={(e) => setSpecificQualities(e.target.value)}
                 />
                 <RadioInputItem
                     id='non-religious-counselor'
                     name={specificQualities}
                     title={t("nonReligiousCounselor")}
-                    value='non_religious_counselor'
-                    checked={specificQualities === "non_religious_counselor"}
+                    value='nonReligiousCounselor'
+                    checked={specificQualities === "nonReligiousCounselor"}
                     onChange={(e) => setSpecificQualities(e.target.value)}
                 />
             </RadioGroup>
