232e111eb3b1d70ec78ff90596059e29964cabbd
A-Abd <abrar.abdulwahed@gmail.com>
2023-04-07 16:29:20 +0300

    refactor: rename contact us url to recommended format

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -2,5 +2,5 @@ export const navigation = [
     { name: "home", href: "/" },
     { name: "blogs", href: "#" },
     { name: "about", href: "#" },
-    { name: "contactUs", href: "/contact_us" },
+    { name: "contactUs", href: "/contact-us" },
 ];
