64f0f0b6000e433cf1db8fec64767fbc1acd3cc9
Jehad Almaliki <yourgitemail@example.com>
2023-04-07 15:00:28 +0300

    feat: use the useRouter hook to get access to the query parameters

--- src/pages/thanks.jsx
+++ src/pages/thanks.jsx
@@ -5,12 +5,11 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import PageIntro from "@/components/PageIntro";
 import Button from "@/components/ui/Button";
-
-export default function Thanks(props) {
+import { useRouter } from "next/router";
+export default function Thanks() {
     const { t } = useTranslation("common");
-    const {
-        subtitle = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-    } = props;
+    const router = useRouter();
+    const { subtitle } = router.query;
     return (
         <>
             <Head>
