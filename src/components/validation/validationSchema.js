a05349b2615aba19abf772af1916fffd2eab7fcf
Jehad Almaliki <yourgitemail@example.com>
2023-04-07 13:25:41 +0300

    refactor: reusable validator schema

---
+++ src/components/validation/validationSchema.js
@@ -0,0 +1,42 @@
+import * as Yup from "yup";
+const schema = Yup.object().shape({
+    firstName: Yup.string()
+        .matches(
+            /^[a-zA-Z\s]*$/,
+            "First name must contain only letters and spaces"
+        )
+        .required("Required"),
+    lastName: Yup.string()
+        .matches(
+            /^[a-zA-Z\s]*$/,
+            "Last name must contain only letters and spaces"
+        )
+        .required("Required"),
+    email: Yup.string()
+        .email("Invalid email format")
+        .matches(
+            /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/,
+            "Invalid email format, must include '@' and '.'"
+        )
+        .required("Required"),
+
+    confirmEmail: Yup.string()
+        .oneOf([Yup.ref("email"), null], "Emails must match")
+        .required("Required"),
+    password: Yup.string()
+        .min(6, "Password must be at least 6 characters long")
+        .matches(
+            /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
+            "Password must contain at least one letter and one number"
+        )
+        .required("Required"),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password"), null], "Passwords must match")
+        .required("Required"),
+    dateOfBirth: Yup.date()
+        .nullable()
+        .max(new Date(), "Date of birth cannot be in the future")
+        .required("Required"),
+});
+
+export default schema;
