a3f9f7957fa6f61f6994c314626a88a2b97986fb
Jehad Almaliki <yourgitemail@example.com>
2023-04-10 00:03:49 +0300

    refactor:  query construction using where

--- src/firebase/getData.js
+++ src/firebase/getData.js
@@ -1,9 +1,9 @@
-import { collection, getDocs, query } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 import { db } from "./config";
 
 export default async function getDocument(doc, criterions) {
-    const q = query(collection(db, doc));
+    const q = query(collection(db, doc), where(...criterions));
     const querySnapshot = await getDocs(q);
     const data = querySnapshot.docs.map((doc) => ({
         id: doc.id,
