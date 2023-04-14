46f6859ebdd40541b53d1f1323688705ee9e6228
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2023-04-14 03:29:58 +0300

    fix: pull changes

---
+++ src/pages/editprofile.jsx
@@ -0,0 +1,159 @@
+import { withTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import FormTitle from "@/components/FormTitle";
+import Button from "@/components/ui/Button";
+import Dropdown from "@/components/ui/Dropdown";
+import Input from "@/components/ui/Input";
+import PreviewProfile from "@/components/ui/PreviewProfile";
+function EditProfile({ t }) {
+    return (
+        <div className='flex flex-wrap'>
+            <div className='ps-5 mb-5'>
+                <PreviewProfile />
+            </div>
+            <div className='container max-w-xl md:shrink-0'>
+                <FormTitle title={t("profileInfo")} />
+                <div className='flex flex-row justify-center my-5 gap-4'>
+                    <Input type='text' name='fullName' label={t("fullName")} />
+                </div>
+                <div className='flex flex-row justify-center my-5'>
+                    <Dropdown
+                        placeholder='select'
+                        data={[
+                            { value: 1, label: "Bacholar" },
+                            { value: 2, label: "Master" },
+                            { value: 3, label: "PhD" },
+                            { value: 4, label: "Deploma" },
+                        ]}
+                        label={t("educationLevel")}
+                    />
+                </div>
+                <div className='flex flex-row justify-center my-5'>
+                    <Input type='text' name='hubbies' label={t("hubbies")} />
+                </div>
+                <div className='flex my-5 gap-10'>
+                    <Input
+                        type='number'
+                        name='FamilySize'
+                        label={t("familySize")}
+                        // placeholder= {"member(s)"}
+                    />
+                    <p className='mt-4'>{t("member(s)")}</p>
+                </div>
+                <div className='flex flex-row justify-center my-5'>
+                    <Dropdown
+                        className='lg:w-8/12'
+                        placeholder='select '
+                        name='gender'
+                        label={t("gender")}
+                        data={[
+                            { value: 1, label: "Female" },
+                            { value: 2, label: "Male" },
+                        ]}
+                    />
+                </div>
+                <div className='flex flex-row justify-center my-5'>
+                    <Input
+                        type='date'
+                        name='BirthDate'
+                        label={t("birthDate")}
+                    />
+                </div>
+                <div className='flex flex-row justify-center my-5'>
+                    <Input type='email' name='email' label={t("Email")} />
+                </div>
+
+                <div className='flex flex-row justify-center my-5'>
+                    <Input
+                        type='text'
+                        name='PhoneNumber'
+                        label={t("phoneNumber")}
+                    />
+                </div>
+                <div className='flex flex-row my-5'>
+                    <label
+                        className={`w-4/12 mt-1 mb-2 whitespace-wrap text-sm md:text-base lg:text-lg flex:me-10 text-light-gray font-light flex:self-center capitalize text-sm md:text-base lg:text-lg`}
+                    >
+                        {t("uploadId")}
+                    </label>
+
+                    <label
+                        for='upload'
+                        className='border rounded-md w-full border-solid self-center border-light-gray/30 text-light-black px-[20px] py-[10px] '
+                    >
+                        <p className='flex justify-end'>+</p>
+                        <input type='file' hidden />
+                    </label>
+                </div>
+                <FormTitle title={t("security")} />
+                <div className='flex flex-row justify-center my-5'>
+                    <Input type='name' name='password' label={t("password")} />
+                </div>
+                <div className='flex flex-row justify-center my-5'>
+                    <Input
+                        type='password'
+                        name='password'
+                        label={t("confirmPassword")}
+                    />
+                </div>
+                <div className='flex xl:flex-row lg:flex-row md:flex-row sm:flex-row flex-col gap-2 my-8'>
+                    <Button
+                        content={t("saveChanges")}
+                        filled='true'
+                        size='small'
+                        radius='md '
+                        shadow='shadow-lg'
+                    />
+                    <Button
+                        content={t("deleteAccount")}
+                        filled='true'
+                        size='small'
+                        radius='md '
+                        shadow='shadow-lg'
+                    />
+                    <Button
+                        content={t("cancel")}
+                        filled='true'
+                        size='small'
+                        radius='md '
+                        shadow='shadow-lg'
+                    />
+                </div>
+
+                <FormTitle title={t("paymentMethods&Tickets")} />
+                <div className='flex gap-10 my-8'>
+                    <div className='flex flex-col gap-5'>
+                        <p>{t("3cardsAdded")}</p>
+                        <Button
+                            content={t("showCards")}
+                            filled='true'
+                            size='small'
+                            radius='md '
+                            shadow='shadow-lg'
+                        />
+                    </div>
+                    <div className='flex flex-col gap-5'>
+                        <p>{t("10ticketsRemaining")}</p>
+                        <Button
+                            content={t("buyTickets")}
+                            filled='true'
+                            size='small'
+                            radius='md '
+                            shadow='shadow-lg'
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common", "profile"])),
+        },
+    };
+}
+export default withTranslation(["profile"])(EditProfile);
