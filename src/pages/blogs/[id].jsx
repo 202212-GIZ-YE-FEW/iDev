4fab5d7c5865456e4c03e2f03510f7d5be83867d
A-Abd <abrar.abdulwahed@gmail.com>
2023-04-17 14:54:55 +0300

    feat: using dynamic routing with ssg in blog

---
+++ src/pages/blogs/[id].jsx
@@ -0,0 +1,103 @@
+import { collection, onSnapshot, query, where } from "@firebase/firestore";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useEffect, useState } from "react";
+
+import RecentBlogSection from "@/components/Homepage/RecentBlogSection";
+import PageIntro from "@/components/PageIntro";
+import Subscribe from "@/components/Subscribe";
+
+import { db } from "@/firebase/config";
+import downloadImage from "@/firebase/getImage";
+import { extractFirstName } from "@/pages/create_blog";
+const Blog = () => {
+    const router = useRouter();
+    const { en_title } = router.query;
+    const title = String(en_title);
+
+    const { i18n } = useTranslation("common");
+    const [blogData, setBlogData] = useState([]);
+    const blogsCollectionRef = collection(db, "blogs");
+    const [imageUrl, setImageUrl] = useState(null);
+
+    useEffect(() => {
+        const q = query(blogsCollectionRef, where("en_title", "==", en_title));
+        let blogs = [];
+        onSnapshot(q, (snapshot) => {
+            blogs = [];
+            snapshot.docs.forEach((doc) => {
+                blogs.push({ ...doc.data(), id: doc.id });
+            });
+            setBlogData(blogs);
+        });
+        const ImageName = extractFirstName(title);
+        downloadImage(ImageName, "blogImages").then((url) => {
+            setImageUrl(url);
+        });
+    }, [title]);
+
+    return (
+        <>
+            <div className=' container parent-div flex flex-col justify-center items-center'>
+                <div className='m-8 child-div-1 w-full max-h-[700px]  '>
+                    {imageUrl && (
+                        <Image
+                            className=' object-cover w-full h-full'
+                            src={imageUrl}
+                            alt='Blog.png'
+                            width={700}
+                            height={250}
+                        />
+                    )}
+                </div>
+
+                {i18n.language == "en" ? (
+                    <>
+                        {" "}
+                        <div className='  child-div-2 flex center text-center sm:justify-center word-wrap:break-word  '>
+                            <div className='  text-center justify-center '>
+                                <PageIntro title={blogData[0]?.en_title} />
+                            </div>
+                        </div>
+                        <div className='child-div-3 flex-none w-full'>
+                            <PageIntro subtitle={blogData[0]?.en_article} />
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        {" "}
+                        <div className='  child-div-2 flex center text-center sm:justify-center word-wrap:break-word  '>
+                            <div className='  text-center justify-center '>
+                                <PageIntro title={blogData[0]?.ar_title} />
+                            </div>
+                        </div>
+                        <div className='child-div-3 flex-none w-full'>
+                            <PageIntro subtitle={blogData[0]?.ar_article} />
+                        </div>
+                    </>
+                )}
+
+                <div className='child-div-3 flex-none w-full m-4'>
+                    <div className='flex justify-start'>
+                        <Subscribe />
+                    </div>
+                </div>
+            </div>
+            <section className='p-8 ' id='recentblog'>
+                <RecentBlogSection />
+            </section>
+        </>
+    );
+};
+
+export default Blog;
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common", "home"])),
+            // Will be passed to the page component as props
+        },
+    };
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import HomePage from "@/components/HomePage";
 
+import getDocument from "@/firebase/getData";
+
 export default function Index() {
     return (
         <>
@@ -10,9 +12,21 @@ export default function Index() {
     );
 }
 export async function getStaticProps({ locale }) {
+    let blogs = [];
+    try {
+        const blogDocs = await getDocument("blogs");
+        if (blogDocs) {
+            blogs = blogDocs.docs.map((blog) => {
+                return { ...blog.data(), id: blog.id };
+            });
+        }
+    } catch (error) {
+        //
+    }
     return {
         props: {
             ...(await serverSideTranslations(locale, "common")),
+            blogs,
             // Will be passed to the page component as props
         },
     };
