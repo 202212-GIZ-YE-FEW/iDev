4ef51a18047bb286f3db8b3106a1688e3a73c664
A-Abd <abrar.abdulwahed@gmail.com>
2023-04-18 16:25:39 +0300

    fix: error

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,6 @@
-const path = require("path");
 module.exports = {
     i18n: {
         defaultLocale: "en",
         locales: ["en", "ar"],
     },
-    localePath: path.resolve("./public/locales"),
 };
