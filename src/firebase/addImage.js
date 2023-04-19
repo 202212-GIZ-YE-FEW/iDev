1982aa4493bb4ee29235edaca3a6a7a20b03be0e
A-Abd <abrar.abdulwahed@gmail.com>
2023-04-19 09:45:26 +0300

    fix: remove alert of uploading blog image

--- src/firebase/addImage.js
+++ src/firebase/addImage.js
@@ -1,8 +1,8 @@
 import { ref, uploadBytes } from "firebase/storage";
+
 import { storage } from "@/firebase/config";
 export default async function uploadImage(imageUpload, imageName, path) {
     if (!imageUpload) return;
     const imageRef = ref(storage, `${path}/${imageName}`);
     await uploadBytes(imageRef, imageUpload);
-    alert("Image uploaded");
 }
