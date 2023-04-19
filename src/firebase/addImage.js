abdf60b3b5347dc8669474aa01e4e66ff38b4e56
A-Abd <abrar.abdulwahed@gmail.com>
2023-04-19 09:43:12 +0300

    fix: remove image extension from naming it in db storage

--- src/firebase/addImage.js
+++ src/firebase/addImage.js
@@ -2,7 +2,7 @@ import { ref, uploadBytes } from "firebase/storage";
 import { storage } from "@/firebase/config";
 export default async function uploadImage(imageUpload, imageName, path) {
     if (!imageUpload) return;
-    const imageRef = ref(storage, `${path}/${imageName}.jpeg`);
+    const imageRef = ref(storage, `${path}/${imageName}`);
     await uploadBytes(imageRef, imageUpload);
     alert("Image uploaded");
 }
