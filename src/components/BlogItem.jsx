941a7141101aa67ee56d43d03911717e3f099d1e
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2023-04-24 21:05:05 +0300

    feat: add hover interaction to blog items

--- src/components/BlogItem.jsx
+++ src/components/BlogItem.jsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { useTranslation, withTranslation } from "next-i18next";
 import { useEffect, useState } from "react";
+import Button from "@/components/ui/Button";
 
 import { storage } from "@/firebase/config";
-function BlogItem({ en_title, ar_title, id, isOdd }) {
+function BlogItem({ en_title, ar_title, id, isOdd, t }) {
     const [imageUrl, setImageUrl] = useState(null);
     const { i18n } = useTranslation();
 
@@ -22,23 +23,36 @@ function BlogItem({ en_title, ar_title, id, isOdd }) {
     return (
         <>
             <div className='relative mx-3'>
-                <Link href={`/blogs/${id}`}>
-                    <div className='h-60'>
-                        <Image
-                            src={
-                                imageUrl ||
-                                "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
-                            }
-                            width={200}
-                            height={140}
-                            alt={`${en_title} thumbnail`}
-                            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                            className='w-full h-full'
+                <div className='h-60'>
+                    <Image
+                        src={
+                            imageUrl ||
+                            "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+                        }
+                        width={200}
+                        height={140}
+                        alt={`${en_title} thumbnail`}
+                        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                        className='w-full h-full'
+                    />
+                </div>
+                <div className='absolute h-full w-full bg-black/70 text-yellow flex flex-col items-center justify-center bottom-0 opacity-0 hover:bottom-0  hover:opacity-100 transition-all duration-1000'>
+                    <p className='mb-5'>
+                        {i18n.language == "ar" ? `${ar_title}` : `${en_title}`}
+                    </p>
+                    <Link href={`/blogs/${id}`}>
+                        <Button
+                            content={t("more")}
+                            // text-transform='capitalize'
+                            filled='false'
+                            size='large'
+                            fontSize='text-sm md:text-xl'
+                            radius='md'
                         />
-                    </div>
-                </Link>
+                    </Link>
+                </div>
 
-                {isOdd ? (
+                {/* {isOdd ? (
                     <div className='w-32 absolute bottom-2 left-2'>
                         <p className='uppercase text-base box-decoration-clone inline leading-7 bg-light-gray text-white px-2'>
                             {i18n.language == "ar"
@@ -50,7 +64,7 @@ function BlogItem({ en_title, ar_title, id, isOdd }) {
                     <p className='uppercase absolute top-2 left-2 text-base bg-light-gray text-white px-2'>
                         {i18n.language == "ar" ? `${ar_title}` : `${en_title}`}
                     </p>
-                )}
+                )} */}
             </div>
         </>
     );
