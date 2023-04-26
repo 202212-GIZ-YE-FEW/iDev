974dac3f5be2cbd9ad4942e71a8d08ebcec851df
MohdBasurra <mohdye96@gmail.com>
2023-04-26 12:02:55 +0300

    fix(change bg-color to white): change background color to white instead of Light cyan

--- src/pages/blogs/blogspage.jsx
+++ src/pages/blogs/blogspage.jsx
@@ -26,30 +26,28 @@ const Blogs = ({ t }) => {
     }, []);
 
     return (
-        <div className='bg-light-cyan'>
-            <div className='container'>
-                <div className='flex flex-wrap justify-around  '>
-                    {blogs.map((blog) => (
-                        <div key={blog.id}>
-                            <div className='w-1/5 md:w-1/3 lg:w-1/5 ml-4 mr-4 '>
-                                <BlogCard
-                                    en_title={blog.body.en_title}
-                                    en_article={
-                                        getFirst80Chars(blog.body.en_article) +
-                                        "..."
-                                    }
-                                    ar_title={blog.body.ar_title}
-                                    ar_article={
-                                        getFirst80Chars(blog.body.ar_article) +
-                                        "..."
-                                    }
-                                    id={blog.id}
-                                    t={t}
-                                />
-                            </div>
+        <div className='container'>
+            <div className='flex flex-wrap justify-around  '>
+                {blogs.map((blog) => (
+                    <div key={blog.id}>
+                        <div className='w-1/5 md:w-1/3 lg:w-1/5 ml-4 mr-4 '>
+                            <BlogCard
+                                en_title={blog.body.en_title}
+                                en_article={
+                                    getFirst80Chars(blog.body.en_article) +
+                                    "..."
+                                }
+                                ar_title={blog.body.ar_title}
+                                ar_article={
+                                    getFirst80Chars(blog.body.ar_article) +
+                                    "..."
+                                }
+                                id={blog.id}
+                                t={t}
+                            />
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
