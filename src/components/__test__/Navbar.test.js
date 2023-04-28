24e2fed51c04307a084276fef73f596a99bd15d2
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-04-28 19:44:04 +0300

    feat: update project test files

--- src/components/__test__/Navbar.test.js
+++ src/components/__test__/Navbar.test.js
@@ -1,8 +1,11 @@
+import mockRouter from 'next-router-mock';
 import renderer from "react-test-renderer";
 
 import Navbar from "../Navbar";
 
+jest.mock('next/router', () => require('next-router-mock'));
 it("renders correctly", () => {
+    mockRouter.setCurrentUrl('/payment');
     const tree = renderer.create(<Navbar />).toJSON();
     expect(tree).toMatchSnapshot();
 });
