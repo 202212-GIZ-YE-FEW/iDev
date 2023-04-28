0f0a95ef852a5badf959bb170884f3723a3958ad
A-Abd <abrar.abdulwahed@gmail.com>
2023-04-28 17:28:37 +0300

    feat: adding message to db

---
+++ src/pages/api/chat.js
@@ -0,0 +1,18 @@
+import addDocument from "@/firebase/addData";
+export default async function handler(req, res) {
+    const { method, body } = req;
+    if (method === "POST") {
+        try {
+            const result = await addDocument("chats", { body });
+            return res.status(200).json({
+                success: 0,
+                message: "Your data submitted successfully",
+                refID: result.result.id,
+            });
+        } catch (error) {
+            return res
+                .status(400)
+                .json({ success: 1, message: "Failed to Submit" });
+        }
+    }
+}
