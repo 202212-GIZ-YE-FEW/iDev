342cc3bc797521c7b85bc9714c79c18628c1e131
Jehad Almaliki <yourgitemail@example.com>
2023-04-28 17:45:31 +0300

    feat: upload img successfullly

--- src/components/ui/PreviewProfile.jsx
+++ src/components/ui/PreviewProfile.jsx
@@ -1,12 +1,30 @@
+import { storage } from "@/firebase/config";
+import { ref, uploadBytes } from "firebase/storage";
+import { React, useState } from "react";
 import Image from "next/image";
 import ProfileEditSVG from "public/edit-profile-icon.svg";
 import ProfilePreviewSVG from "public/profile-icon.svg";
-import { React, useState } from "react";
+import uploadImage from "@/firebase/addImage";
+import { useAuth } from "../context/AuthContext";
 export default function PreviewProfile() {
     const [imgfile, uploadimg] = useState("");
-    const imgFilehandler = (e) => {
-        uploadimg(URL.createObjectURL(e.target.files[0]));
+    const { user } = useAuth();
+
+    const imgFilehandler = async (e) => {
+        const file = e.target.files[0];
+        const imageName = file.name;
+        const userId = user.uid;
+        const path = "ProfilesImages/" + userId;
+        uploadimg(URL.createObjectURL(file));
+
+        try {
+            await uploadImage(file, imageName, path);
+            console.log("File uploaded successfully!");
+        } catch (error) {
+            console.error(error);
+        }
     };
+
     return (
         <>
             <div className='relative inline-flex justify-center w-[14rem] h-[14rem] lg:w-[20rem] lg:h-[20rem] xl:w-[18rem] xl:h-[18rem] rounded-full'>
