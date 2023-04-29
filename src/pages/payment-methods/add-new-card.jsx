cbb3134923c869ec3b460e4ff6143026d31b73f8
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-04-29 20:42:50 +0300

    fix: validate payment card type

--- src/pages/payment-methods/add-new-card.jsx
+++ src/pages/payment-methods/add-new-card.jsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { withTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useState } from "react";
+import { BsFillCreditCardFill } from "react-icons/bs";
+import { FaCcMastercard, FaCcVisa } from "react-icons/fa";
 
 import "react-multi-carousel/lib/styles.css";
 
@@ -24,6 +26,20 @@ function AddNewCard({ t }) {
         label: country.name,
     }));
 
+    const [formData, setFormData] = useState({});
+    const [formErrors, setFormErrors] = useState({});
+
+    const {
+        cardNumber = "cardNumber",
+        expiryDate = "expiryDate",
+        cvv = "cvv",
+        nameOnCard = "nameOnCard",
+        country = "country",
+        zipCode = "zipCode",
+        city = "city",
+        address = "address",
+    } = {};
+
     const handleCountryChange = (e) => {
         const countryName = e.target.value;
         const cities = getCitiesOfCountry(countryName).map((city) => ({
@@ -31,6 +47,39 @@ function AddNewCard({ t }) {
             label: city.name,
         }));
         setCities(cities);
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const cardType = (cardNumber) => {
+        if (!cardNumber) {
+            return null;
+        }
+        if (cardNumber.startsWith("4")) {
+            return <FaCcVisa />;
+        } else if (cardNumber.startsWith("5")) {
+            return <FaCcMastercard />;
+        } else {
+            return <BsFillCreditCardFill />;
+        }
+    };
+
+    const handleCardNumberChange = (e) => {
+        const { value } = e.target;
+        const cardNumber = value
+            .replace(/\s+/g, "")
+            .replace(/[^0-9]/gi, "")
+            .replace(/\s/g, "")
+            .match(/.{1,4}/g)
+            ?.join(" ");
+        setFormData({ ...formData, cardNumber });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
     };
 
     return (
@@ -43,19 +92,20 @@ function AddNewCard({ t }) {
                     title={t("addCardDetails")}
                     subtitle={t("addCardDetailsDesc")}
                 />
-                <div className='grid grid-cols-1 xl:grid-cols-2 my-10 gap-28'>
+                <div className='grid grid-cols-1 xl:grid-cols-2 my-10'>
                     <form
                         autoComplete='off'
-                        className='flex flex-col md:flex-row md:justify-between xl:flex-col gap-x-5 gap-y-16'
+                        className='flex flex-col md:flex-row md:justify-between xl:flex-col gap-x-5 mb-16 gap-y-6'
+                        onSubmit={handleSubmit}
                     >
                         <div className='min-w-fit md:min-w-[25rem] lg:min-w-[30rem]'>
                             <div className='grid grid-cols-1 xl:grid-cols-2 my-10 gap-24'>
                                 <div className='flex flex-col gap-5'>
-                                    <div className='flex-col mt-[0.8rem] mb-3'>
-                                        <span className='mt-1 mb-2 whitespace-wrap md:text-base lg:text-lg flex:me-10 text-light-gray font-light flex:self-center capitalize text-sm flex-1'>
+                                    <div className='flex-col mt-[0.8rem] mb-2'>
+                                        <span className='mb-2 whitespace-wrap md:text-base lg:text-lg flex:me-10 text-light-gray font-light flex:self-center capitalize text-sm flex-1'>
                                             {t("supportedCardTypes")}
                                         </span>
-                                        <div className='flex'>
+                                        <div className='flex mt-1'>
                                             <div className='w-full border border-cyan px-5 rounded-md rounded-e-none text-center'>
                                                 <span className='mt-1 mb-2 whitespace-wrap md:text-base lg:text-lg text-cyan'>
                                                     Visa
@@ -70,23 +120,35 @@ function AddNewCard({ t }) {
                                     </div>
                                     <div className='flex-col mt-[0.8rem]'>
                                         <Input
-                                            field='Card Number'
+                                            field={cardNumber}
                                             label={t("cardNumber")}
                                             placeholder='4287  8874 9511 3263'
                                             type='text'
                                             name='cardNumber'
                                             inputHeightSize='h-10'
+                                            value={formData.cardNumber || ""}
+                                            onChange={handleCardNumberChange}
+                                            error={formErrors.cardNumber}
+                                            maxLength='19'
+                                            suffixIcon={cardType(
+                                                formData.cardNumber
+                                            )}
                                             t={t}
                                         />
                                     </div>
                                     <div className='flex space-x-[0.7rem] rtl:space-x-reverse'>
-                                        <div className='flex-col mt-[0.8rem]'>
+                                        <div className='flex-col mt-[0.8rem] w-1/2'>
                                             <Input
                                                 field='Expiry Date'
                                                 label={t("expiryDate")}
-                                                type='date'
+                                                type='month'
                                                 name='expiryDate'
                                                 inputHeightSize='h-10'
+                                                value={
+                                                    formData.expiryDate || ""
+                                                }
+                                                onChange={handleInputChange}
+                                                error={formErrors.expiryDate}
                                                 t={t}
                                             />
                                         </div>
@@ -98,6 +160,9 @@ function AddNewCard({ t }) {
                                                 type='text'
                                                 name='cvv'
                                                 inputHeightSize='h-10'
+                                                value={formData.cvv || ""}
+                                                onChange={handleInputChange}
+                                                error={formErrors.cvv}
                                                 maxLength='3'
                                                 t={t}
                                             />
@@ -111,6 +176,9 @@ function AddNewCard({ t }) {
                                             type='text'
                                             name='nameOnCard'
                                             inputHeightSize='h-10'
+                                            value={formData.nameOnCard || ""}
+                                            onChange={handleInputChange}
+                                            error={formErrors.nameOnCard}
                                             t={t}
                                         />
                                     </div>
@@ -133,6 +201,9 @@ function AddNewCard({ t }) {
                                             type='text'
                                             name='zipCode'
                                             inputHeightSize='h-10'
+                                            value={formData.zipCode || ""}
+                                            onChange={handleInputChange}
+                                            error={formErrors.zipCode}
                                             t={t}
                                         />
                                     </div>
@@ -150,8 +221,11 @@ function AddNewCard({ t }) {
                                             label={t("address")}
                                             placeholder='509 Adele Mills Suite 833'
                                             type='text'
-                                            name='zipCode'
+                                            name='address'
                                             inputHeightSize='h-10'
+                                            value={formData.address || ""}
+                                            onChange={handleInputChange}
+                                            error={formErrors.address}
                                             t={t}
                                         />
                                     </div>
