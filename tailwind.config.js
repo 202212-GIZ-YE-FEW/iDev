140dcbcfb14a88d9a45c5c00b2486b4887b5d1b3
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-05-01 13:08:41 +0300

    feat: enable dark mode for tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     content: ["./src/pages/**/*.{js,jsx}", "./src/components/**/*.{js,jsx}"],
     purge: ["./src/pages/**/*.{js,jsx}", "./src/components/**/*.{js,jsx}"],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: "class", // or 'media' or 'class'
     variants: {
         extend: {},
     },
