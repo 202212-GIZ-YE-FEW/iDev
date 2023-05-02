ba05394da792e03ec84976a6ad81441c48656395
Jehad Almaliki <yourgitemail@example.com>
2023-05-02 22:27:32 +0300

    feat: edit the fullname validation

--- src/utils/validationSchemaProfile.js
+++ src/utils/validationSchemaProfile.js
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 
 const schema = Yup.object().shape({
-    fullName: Yup.string()
+    Fullname: Yup.string()
         .matches(/^[\u0621-\u064A\u0660-\u0669 a-zA-Z\s]+$/, "letterSpace")
         .required("required"),
     hubbies: Yup.string()
