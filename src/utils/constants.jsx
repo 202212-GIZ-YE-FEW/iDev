b204e660277c8d360f1f8adb4905d3c03d5ba6e2
MohdBasurra <mohdye96@gmail.com>
2023-05-04 22:05:02 +0300

    feat(add careers button to footer): add button in footer that take you to careers page

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,6 +1,16 @@
+// for Navbar
 export const navigation = [
     { name: "home", href: "/" },
     { name: "blogs", href: "blogs" },
     { name: "about", href: "#" },
     { name: "contactUs", href: "/contact-us" },
+];
+
+// for Footer
+export const navigationFooter = [
+    { name: "home", href: "/" },
+    { name: "blogs", href: "blogs" },
+    { name: "about", href: "#" },
+    { name: "contactUs", href: "/contact-us" },
+    { name: "careers", href: "/careers" },
 ];
