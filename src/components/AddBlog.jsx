b8ce0d4e5ba0a7e3e6b35fafe2f904ef4f8f6583
MohdBasurra <mohdye96@gmail.com>
2023-05-10 20:19:47 +0300

    feat(create new component): create new Component to push user to create Blogs

---
+++ src/components/AddBlog.jsx
@@ -0,0 +1,41 @@
+import Button from "@/components/ui/Button";
+import PageIntro from "@/components/PageIntro";
+import { withTranslation } from "next-i18next";
+import { useAuth } from "@/components/context/AuthContext";
+
+const AddBlog = ({ t }) => {
+    const { authenticated } = useAuth();
+    const handleClick = () => {
+        const router = require("next/router").default;
+        router.push({
+            pathname: "/blogs/create",
+        });
+    };
+
+    return (
+        <>
+            {authenticated ? (
+                <>
+                    <div className='  pt-8 pb-6'>
+                        <PageIntro
+                            title={t("ShareYourBlogPost")}
+                            subtitle={t("submitYourBlog")}
+                        />
+                        <Button
+                            content={t("addToCareersList")}
+                            textTransform='uppercase'
+                            filled='true'
+                            size='large'
+                            fontSize='text-lg md:text-xl lg:text-2xl'
+                            radius='md'
+                            onClick={handleClick}
+                        />
+                    </div>
+                </>
+            ) : (
+                <></>
+            )}
+        </>
+    );
+};
+export default withTranslation("blog")(AddBlog);
